2a482bb003cd190b7c82014e4b4bbc9a3683d0cd
akkoumis <akkoumis@ece.auth.gr>
2019-03-26 19:51:48 +0200

    Added My Vehicles Lists

--- app/src/main/java/cy/com/talaiporoi/authauto/MyVehiclesActivity.java
+++ app/src/main/java/cy/com/talaiporoi/authauto/MyVehiclesActivity.java
@@ -1,13 +1,25 @@
 package cy.com.talaiporoi.authauto;
 
+import android.content.res.Resources;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyVehiclesActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private MyVehiclesAdapter adapter;
+    private List<Vehicle> vehicleList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +37,89 @@ public class MyVehiclesActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        vehicleList = new ArrayList<>();
+        adapter = new MyVehiclesAdapter(this, vehicleList);
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 1);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(1, dpToPx(10), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+
+        prepareVehicles();
+
+    }
+
+    /**
+     * Adding few albums for testing
+     */
+    private void prepareVehicles() {
+        int[] photos = new int[]{
+                R.mipmap.tesla_model_x,
+                R.mipmap.tesla_model_s,
+                R.mipmap.tesla_model_3
+        };
+
+        Vehicle a = new Vehicle("Tesla", "Model X", photos[0]);
+        vehicleList.add(a);
+
+        a = new Vehicle("Tesla", "Model S", photos[1]);
+        vehicleList.add(a);
+
+        a = new Vehicle("Tesla", "Model 3", photos[2]);
+        vehicleList.add(a);
+
+
+        adapter.notifyDataSetChanged();
+    }
+
+    /**
+     * RecyclerView item decoration - give equal margin around grid item
+     */
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
     }
 
 }
---
+++ app/src/main/java/cy/com/talaiporoi/authauto/MyVehiclesAdapter.java
@@ -0,0 +1,69 @@
+package cy.com.talaiporoi.authauto;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class MyVehiclesAdapter extends RecyclerView.Adapter<MyVehiclesAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private List<Vehicle> vehicleList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, subtitle;
+        public ImageView photo;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.text_title);
+            subtitle = (TextView) view.findViewById(R.id.text_subtitle);
+            photo = (ImageView) view.findViewById(R.id.photo);
+            //overflow = (ImageView) view.findViewById(R.id.overflow);
+        }
+    }
+
+
+    public MyVehiclesAdapter(Context mContext, List<Vehicle> vehicleList) {
+        this.mContext = mContext;
+        this.vehicleList = vehicleList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.vehicle_card, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
+        Vehicle vehicle = vehicleList.get(position);
+        holder.title.setText(vehicle.getManufacturer());
+        holder.subtitle.setText(vehicle.getModel());
+
+        // loading vehicle cover using Glide library
+        Glide.with(mContext).load(vehicle.getPhoto()).into(holder.photo);
+
+        /*holder.overflow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu(holder.overflow);
+            }
+        });*/
+    }
+
+    @Override
+    public int getItemCount() {
+        return vehicleList.size();
+    }
+
+}
