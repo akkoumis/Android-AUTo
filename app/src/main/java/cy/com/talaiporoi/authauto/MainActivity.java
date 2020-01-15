3b30892103c99e27d8e5395ac10dcf1ff74230f3
akkoumis <akkoumis@ece.auth.gr>
2020-01-15 15:33:00 +0200

    Added library
    problems with queries

--- app/src/main/java/cy/com/talaiporoi/authauto/MainActivity.java
+++ app/src/main/java/cy/com/talaiporoi/authauto/MainActivity.java
@@ -2,27 +2,32 @@ package cy.com.talaiporoi.authauto;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+
+    private Connection mysqlConnection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,13 +58,107 @@ public class MainActivity extends AppCompatActivity
                 .findFragmentById(R.id.mapView);
         mapFragment.getMapAsync(this);*/
 
+        connectToDB();
+
+
+    }
+
+    private void login() {
+        Log.d("koumis", "Executing SELECT...");
+        Statement statement = mysqlConnection.createStatement();
+        statement.executeQuery("show schemas", new IResultInterface() {
+            @Override
+            public void executionComplete(ResultSet resultSet) {
+                //Toast toast = Toast.makeText(getApplicationContext(), "Number of rows = " + resultSet.getNumRows(), Toast.LENGTH_SHORT);
+                //toast.show();
+                Log.d("koumis", "Select query Num of rows = " + resultSet.getNumRows());
+            }
+
+            @Override
+            public void handleInvalidSQLPacketException(InvalidSQLPacketException ex) {
+                Log.e("koumis", "InvalidSQLPacketException Error");
+            }
+
+            @Override
+            public void handleMySQLException(MySQLException ex) {
+                Log.e("koumis", "MySQLException Error");
+
+            }
+
+            @Override
+            public void handleIOException(IOException ex) {
+                Log.e("koumis", "IOException Error");
+
+            }
+
+            @Override
+            public void handleMySQLConnException(MySQLConnException ex) {
+                Log.e("koumis", "MySQLConnException Error");
+
+            }
+
+            @Override
+            public void handleException(Exception ex) {
+                Log.e("koumis", "Exception Error");
+
+            }
+
+        });
+    }
+
+    private void connectToDB() {
+        Log.d("koumis", "DB Connecting...");
+
+        mysqlConnection = new Connection("10.0.2.2", "root",
+                "root", 3306, "authauto", new IConnectionInterface() {
+            @Override
+            public void actionCompleted() {
+                //You are now connected to the database
+                //Toast toast = Toast.makeText(getApplicationContext(), "DB Connected", Toast.LENGTH_SHORT);
+                //toast.show();
+                Log.d("koumis", "DB Connected!");
+                login();
+            }
+
+            @Override
+            public void handleInvalidSQLPacketException(InvalidSQLPacketException e) {
+                //Handle the error
+            }
+
+            @Override
+            public void handleMySQLException(MySQLException e) {
+                //Handle the error
+            }
+
+            @Override
+            public void handleIOException(IOException e) {
+                //Handle the error
+            }
+
+            @Override
+            public void handleMySQLConnException(MySQLConnException e) {
+                //Handle the error
+                Log.e("koumis", "Connection Error");
+            }
+
+            @Override
+            public void handleException(Exception e) {
+                //Handle the error
+            }
+        });
+        //The below line isn't required, however, the MySQL action, whether connecting or executing a statement
+        //(basically anything that uses the this connection object) does its action in a thread, so the call
+        //back you receive will still be in its thread. If you are performing a GUI operation in your callback you need to
+        //switch the main thread. You can either do this yourself when required, or pass true as the first parameter
+        //so that when you receive the call back it is already switched to the main thread
+        mysqlConnection.returnCallbackToMainThread(true, MainActivity.this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         // Add a marker in Sydney, Australia,
         // and move the map's camera to the same location.
-        LatLng thessaloniki = new LatLng(40.636 ,22.942 );
+        LatLng thessaloniki = new LatLng(40.636, 22.942);
         googleMap.addMarker(new MarkerOptions().position(thessaloniki));//.title("Marker in Sydney"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(thessaloniki));
     }
@@ -118,9 +217,9 @@ public class MainActivity extends AppCompatActivity
     /**
      * Called when the user taps the My vehicles button
 
-    public void viewMyVehicles(MenuItem item) {
-        Intent intent = new Intent(this, MyVehiclesActivity.class);
+     public void viewMyVehicles(MenuItem item) {
+     Intent intent = new Intent(this, MyVehiclesActivity.class);
 
-        startActivity(intent);
-    }*/
+     startActivity(intent);
+     }*/
 }
